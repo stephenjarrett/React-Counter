451970836a0399506e7d58abae63b34fd8dac7bc
Stephen <smjarr89@gmail.com>
2018-07-18 10:08:38 -0400

    changed Counter from a react fn to a class fn for dynamic counting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import Counter from './Counter';
 
+const handleClick = () => {
+	console.log('Clicked!');
+	counterElements.push(
+		<Counter finalValue='100' initialValue=
+		{parseInt((Math.random()*100),10)-1} />
+	)
+	console.log(counterElements);
+}
+
+function convertNumToCounter(value) {
+	return (
+		<Counter initialValue={value} finalValue='100' />
+	);
+}
+
+// Not quite the React way... global variables
+let counterValues = [5,23,54,12,32];
+let counterElements = counterValues.map(convertNumToCounter);
+
+
+// App is a React Component
 const App = () => {
 	return (
 
 		<div className="container">
-			<button>+</button>
+			<button className='button' onClick={handleClick}>+</button>
 			<div className="counter-box">
-				<Counter finalValue='100' initialValue='5' />
-				<Counter finalValue='100' initialValue='23'/>
-				<Counter finalValue='100' initialValue='54'/>
-				<Counter finalValue='100' initialValue='12'/>
-				<Counter finalValue='100' initialValue='32'/>
-			</div>
-			
-		
+				{counterElements}
+			</div>		
 		</div>
 );
 };
--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,57 @@
 import React from 'react';
 
-// Counter is a React component
-const Counter = ({finalValue, initialValue}) => {
-    return(
-        <div className="counter">
-        {initialValue}
-        </div>
-    );
-};
+// Rule #1: extend React.Component
+class Counter extends React.Component {
+    // Rule #4: Accept props in the
+    // constructor method
+    constructor(props) {
+        super(props);
+        
+        // State is *my* stuff
+        // It is always an object
+        this.state = {
+            currentValue: props.initialValue
+        };
+        
+        setInterval(this._increaseValue, 1000);
+    }
+
+    // Always write helper fns as arrow 
+    // fns
+    // Keeps bugs away.. start custom fns
+    // with _ first
+    _increaseValue = () => {
+        // Calculate the new current value
+        let newCurrentValue = this.state.currentValue + 1;
+        // Then set the new currentValue in state
+        // but I must call this.setState
+        //  I cannot alter this.state directly
+
+        this.setState({
+            currentValue: newCurrentValue
+        });
+    }
+    
+    // Rule #2: must have render method
+    render() {
+        //  Rule #3: must return some JSX
+        //  or a call to React.createElement
+        return (
+            <div className='counter'>
+                {this.state.currentValue}
+            </div>
+        );
+    }
+}
+
+// old style without classes
+// // Counter is a React component
+// const Counter = ({finalValue, initialValue}) => {
+//     return(
+//         <div className="counter">
+//         {initialValue}
+//         </div>
+//     );
+// };
 
 export default Counter;
